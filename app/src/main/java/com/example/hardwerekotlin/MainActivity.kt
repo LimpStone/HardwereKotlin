0d6fd0ec3eb72af23e7328fb7afcd5210a64672f
LimpStone <josueeslomejor@live.com>
2024-03-07 10:01:02 -0600

    moar

--- app/src/main/java/com/example/hardwerekotlin/MainActivity.kt
+++ app/src/main/java/com/example/hardwerekotlin/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.hardwerekotlin
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnQr = findViewById<Button>(R.id.qr)
-
+        val gps = findViewById<Button>(R.id.gps)
+        var cordenadas = findViewById<TextView>(R.id.textView2)
         // Register the launcher and result handler
         val barcodeLauncher = registerForActivityResult<ScanOptions, ScanIntentResult>(
             ScanContract()
@@ -62,5 +70,18 @@ class MainActivity : AppCompatActivity() {
             android.Manifest.permission.ACCESS_FINE_LOCATION,
             android.Manifest.permission.ACCESS_COARSE_LOCATION))
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        gps.setOnClickListener{
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener{ task ->
+                    if(task.isSuccessful){
+
+                        cordenadas.text  = "Latitud: "+ task.result.latitude.toString()+" Longitud "+task.result.longitude.toString()
+                    }else{
+
+                    }
+                    // Got last known location. In some rare situations this can be null.
+                }
+        }
     }
 }
